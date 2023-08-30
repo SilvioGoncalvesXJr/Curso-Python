d88ce49a7ff77fcbee450a4b72dabf2cae309701
Silvio Goncalves <silviogxjr@hotmail.com>
2023-08-30 13:54:36 -0300

    Att

--- range_for.py
+++ range_for.py
@@ -3,7 +3,19 @@ for + range
 range -> range(start, stop, step)
 """
 
-numeros = range(0, 100,8)
+numerosImpares = range(1, 100,2)
 
-for numero in numeros:
-    print(numero)
+impar = []
+
+for numero in numerosImpares:
+    impar.append(numero) 
+print(impar)
+
+numerosPares = range(0, 100, 2)
+
+pares = []
+
+for numeropar in numerosPares:
+    pares.append(numeropar)
+    
+print(pares)
