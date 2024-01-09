f6c114e9ff4d1ea840493809b8e7aec05070c13d
Silvio Goncalves <silvio.goncalves@aluno.uece.br>
2024-01-09 16:36:58 -0300

    Exercício Lista

--- Exercícios/Exercício_Lista_de_Compras.py
+++ Exercícios/Exercício_Lista_de_Compras.py
@@ -12,30 +12,48 @@ lista = []
 flag = True
 while flag == True:
     opcao = input("Selecione uma opção:\n 1 - Inserir\n 2 - Apagar\n 3 - Listar\n 4 - Sair\n")
+    
     if opcao == '1':
         item = input("O que deseja inserir?")
         lista.append(item)
         print("Item adicionado com sucesso")
         time.sleep(2)
         os.system('cls')
+    
     elif opcao == '2':
-        print(lista)
-        valor = input("O que deseja pagar da lista?")
-        if valor in lista:
-            lista.remove(valor)
-            print("item apagado com sucessoAS")
-        else:
-            print("Valor não encontrado")
-        
-        time.sleep(2)
-        os.system('cls')
+        # print(lista)
+        # valor = input("O que deseja pagar da lista?")
+        # if valor in lista:
+        #     lista.remove(valor)
+        #     print("item apagado com sucessoAS")
+        # else:
+        #     print("Valor não encontrado")
+        if len(lista) == 0:
+            print("Lista vazia. Por favor, adicione itens antes")
+            time.sleep(2)
+            os.system('cls')
+        else:    
+            for item in enumerate(lista):
+                print(item)
+            indice = int(input("Qual o índice do item que deseja excluir? Ex. 1, 2, 3, por exemplo\n"))
+            try:
+                del lista[indice]
+                #Aqui, tentei usar a função delete(), mas não deu certo pq ela só funciona por valor
+                #Já o del remove por index propriamente
+                print("Removido com sucesso")
+            except:
+                print("Valor não identificado. Por favor, tente novamente")
+            time.sleep(2)
+            os.system('cls')
+    
     elif opcao == '3':
-        for item, valor in enumerate(lista):
-            print(f'Segue a lista:\n {item} {valor}')
-        time.sleep(8)
+        for item in enumerate(lista):
+            print(item)
+        time.sleep(4)
         os.system('cls')
+    
     elif opcao == '4':
-        print('ok4')
+        print('Obrigado')
         flag = False
         time.sleep(2)
         os.system('cls')
