5b2b93df19b04f55039b8d16169a7c889e8eca33
Silvio Goncalves <silvio.goncalves@aluno.uece.br>
2024-01-22 22:45:36 -0300

    Att

---
+++ Exercícios/Gerar_1Dig_CPF.py
@@ -0,0 +1,87 @@
+"""
+Calculo do primeiro dígito do CPF
+CPF: 746.824.890-70
+Colete a soma dos 9 primeiros dígitos do CPF
+multiplicando cada um dos valores por uma
+contagem regressiva começando de 10
+
+Ex.:  746.824.890-70 (746824890)
+   10  9  8  7  6  5  4  3  2
+*  7   4  6  8  2  4  8  9  0
+   70  36 48 56 12 20 32 27 0
+
+Somar todos os resultados: 
+70+36+48+56+12+20+32+27+0 = 301
+Multiplicar o resultado anterior por 10
+301 * 10 = 3010
+Obter o resto da divisão da conta anterior por 11
+3010 % 11 = 7
+Se o resultado anterior for maior que 9:
+    resultado é 0
+contrário disso:
+    resultado é o valor da conta
+
+O primeiro dígito do CPF é 7
+"""
+
+lista = []
+listaint = []
+
+cpf = input("Informe um cpf válido: ")
+
+
+for i in cpf:
+    lista.append(i)
+print(lista)
+
+for i in lista:
+    if i == '.' or i == '-':
+        lista.remove(i)
+print(lista)
+
+lista.pop()
+lista.pop()
+
+for i in lista:
+    listaint.append(int(i))
+print(listaint)
+
+valor = 10
+listaintAtt = []
+for i in listaint:
+    listaintAtt.append(i*valor)
+    valor -= 1
+print(listaintAtt)
+
+valores = 0
+for i in listaintAtt:
+    valores += i
+print(valores)
+
+valores = valores * 10
+print(valores)
+
+valores = valores % 11
+print(valores)
+
+primeiro_digito = 0 if valores > 9 else valores
+
+print(f'O primeiro dígito é {primeiro_digito}')
+
+# del lista[3]
+# print(lista)
+# del lista[6]
+# print(lista)
+# del lista[9]
+# print(lista)
+# cpf_separado = cpf.split('.') 
+# divisao_travessao = cpf_separado.pop(2)
+# divisao_travessao = divisao_travessao.split('-')
+
+# for i in divisao_travessao:
+#     cpf_separado.append(i)
+# print(cpf_separado)
+
+# for i in cpf_separado:
+#     lista.append(int(i))
+# print(lista)
