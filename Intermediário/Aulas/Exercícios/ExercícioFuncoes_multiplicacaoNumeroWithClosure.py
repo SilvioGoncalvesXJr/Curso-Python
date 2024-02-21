99ff06e90fe79482e4c8beac7e90257ba0a490da
Silvio Goncalves <silvio.goncalves@aluno.uece.br>
2024-02-20 21:02:02 -0300

    Att

--- Intermediário/Aulas/Exercícios/ExercícioFuncoes_multiplicacaoNumeroWithClosure.py
+++ Intermediário/Aulas/Exercícios/ExercícioFuncoes_multiplicacaoNumeroWithClosure.py
@@ -43,6 +43,21 @@ triplo = multiplicacao(numero, 3)
 quadro = multiplicacao(numero, 4)
 print(f'O resultado da duplicação de {numero} é {duplicacao()}\nO seu triplo é {triplo()}\nAgora o seu quadruplo é {quadro()}')
 
+#Versão Professor
+
+def criar_multiplicador(multiplicador):
+    def multiplicar(numero):
+        return numero * multiplicador
+    return multiplicar
+
+duplicar = criar_multiplicador(2)
+triplicar= criar_multiplicador(3)
+quadruplicar = criar_multiplicador(4)
+
+print(duplicar(2))
+print(triplicar(2))
+print(quadruplicar(4))
+
 
 
 
